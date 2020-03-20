40e73e97f0507f7577259e8f59a3963f28886195
lampajr <lampa9559@gmail.com>
2020-03-20 15:53:39 +0100

    dairy contract initialization added

--- smartcontracts/fabric/dairy/javascript/lib/dairy.js
+++ smartcontracts/fabric/dairy/javascript/lib/dairy.js
@@ -2,179 +2,243 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use strict';
+"use strict";
 
-const { Contract } = require('fabric-contract-api');
+const { Contract } = require("fabric-contract-api");
 
 /**
  * This contract allows all milk supply chain's involved actors to log their operations in a persistent way
  */
 class diary extends Contract {
-  /**
-   * Register a new milk batch in the supply chain ecosystem
-   * @param {string} batchId identifier of the batch
-   * @param {string} ranchName name of the ranch where the milk was produced
-   * @param {string} location place where the ranch is located
-   * @param {number} amount amount of milk in the batch
-   */
-  static async registerMilkBatch(ctx, batchId, ranchName, location, amount) {
-    console.info('============= START : registerMilkBatch ===========');
+    constructor() {
+        super("Dairy products supply chain contract");
+    }
 
-    const batchesRegister = 'bacthesRegister';
-    const newBatch = {
-      ranchName: ranchName,
-      location: location,
-      amout: amount,
-    };
-    const batchesAsBytes = await ctx.stub.getState(batchesRegister);
-    const batches = JSON.parse(batchesAsBytes.toString());
-    batches[batchId] = newBatch;
-    await ctx.stub.putState(batchesRegister, Buffer.from(JSON.stringify(batches)));
-    ctx.stub.setEvent(
-      'milkBatchRegistered',
-      Buffer.from(
-        JSON.stringify({
-          [batchId]: newBatch,
-        }),
-      ),
-    );
+    async setup(ctx) {
+        console.info("============= START : Initialize Ledger ===========");
 
-    console.info('============= END   : registerMilkBatch ===========');
-  }
+        await ctx.stub.putState(
+            "bacthesRegister",
+            Buffer.from(JSON.stringify({}))
+        );
+        await ctx.stub.putState(
+            "batchesTransports",
+            Buffer.from(JSON.stringify({}))
+        );
+        await ctx.stub.putState(
+            "cartonsRegister",
+            Buffer.from(JSON.stringify({}))
+        );
+        await ctx.stub.putState(
+            "cartonsTransports",
+            Buffer.from(JSON.stringify({}))
+        );
+        await ctx.stub.putState(
+            "cartonsInventory",
+            Buffer.from(JSON.stringify({}))
+        );
+        await ctx.stub.putState(
+            "cartonsSales",
+            Buffer.from(JSON.stringify([]))
+        );
 
-  /**
-   * Register that a milk batch was transported, assumed that one entire milk batch is transported at once
-   * @param {string} logisticsFirmName name of the firm that has transported the milk batch
-   * @param {string} toLocation place where the batch was transported to
-   * @param {string} batchId identifier of the milk batch
-   */
-  async transportMilkBatch(ctx, logisticsFirmName, toLocation, batchId) {
-    console.info('============= START : transportMilkBatch ===========');
+        console.info("============= END : Initialize Ledger ===========");
+    }
 
-    const batchesTransports = 'batchesTransports';
-    const newTransport = {
-      logisticsFirmName: logisticsFirmName,
-      toLocation: toLocation,
-    };
-    const transportsAsBytes = await ctx.stub.getState(batchesTransports);
-    const transports = JSON.parse(transportsAsBytes.toString());
-    transports[batchId] = newTransport;
-    await ctx.stub.putState(batchesTransports, Buffer.from(JSON.stringify(transports)));
-    ctx.stub.setEvent(
-      'milkBatchTransported',
-      Buffer.from(
-        JSON.stringify({
-          [batchId]: newTransport,
-        }),
-      ),
-    );
+    /**
+     * Register a new milk batch in the supply chain ecosystem
+     * @param {string} batchId identifier of the batch
+     * @param {string} ranchName name of the ranch where the milk was produced
+     * @param {string} location place where the ranch is located
+     * @param {number} amount amount of milk in the batch
+     */
+    async registerMilkBatch(ctx, batchId, ranchName, location, amount) {
+        console.info("============= START : registerMilkBatch ===========");
 
-    console.info('============= END   : transportMilkBatch ===========');
-  }
+        const batchesRegister = "bacthesRegister";
+        const newBatch = {
+            ranchName: ranchName,
+            location: location,
+            amout: amount
+        };
+        const batchesAsBytes = await ctx.stub.getState(batchesRegister);
+        const batches = JSON.parse(batchesAsBytes.toString());
+        batches[batchId] = newBatch;
+        await ctx.stub.putState(
+            batchesRegister,
+            Buffer.from(JSON.stringify(batches))
+        );
+        ctx.stub.setEvent(
+            "milkBatchRegistered",
+            Buffer.from(
+                JSON.stringify({
+                    [batchId]: newBatch
+                })
+            )
+        );
 
-  /**
-   * Register a new milk carton, assumed that at the processing facility, milk from multiple batches is mixed, therefore, a single milk carton could contain milk of multiple batches
-   * @param {string} processorFirmName name of the firm that has processed the milk carton
-   * @param {string} location place where the processing has taken place
-   * @param {string} originBatchIds identifiers of the batches from which the milk was taken
-   * @param {string} cartonId identifier of the new milk carton
-   */
-  async registerMilkCarton(ctx, processorFirmName, location, originBatchIds, cartonId) {
-    console.info('============= START : registerMilkCarton ===========');
+        console.info("============= END   : registerMilkBatch ===========");
+    }
 
-    const cartonsRegister = 'cartonsRegister';
-    const newCarton = {
-      processorFirmName: processorFirmName,
-      location: location,
-      originBatchIds: originBatchIds,
-    };
-    const cartonsAsBytes = await ctx.stub.getState(cartonsRegister);
-    const cartons = JSON.parse(cartonsAsBytes.toString());
-    cartons[cartonId] = newCarton;
-    await ctx.stub.putState(cartonsRegister, Buffer.from(JSON.stringify(cartons)));
-    ctx.stub.setEvent(
-      'cartonRegistered',
-      Buffer.from(
-        JSON.stringify({
-          [cartonId]: newCarton,
-        }),
-      ),
-    );
+    /**
+     * Register that a milk batch was transported, assumed that one entire milk batch is transported at once
+     * @param {string} logisticsFirmName name of the firm that has transported the milk batch
+     * @param {string} toLocation place where the batch was transported to
+     * @param {string} batchId identifier of the milk batch
+     */
+    async transportMilkBatch(ctx, logisticsFirmName, toLocation, batchId) {
+        console.info("============= START : transportMilkBatch ===========");
 
-    console.info('============= END   : registerMilkCarton ===========');
-  }
+        const batchesTransports = "batchesTransports";
+        const newTransport = {
+            logisticsFirmName: logisticsFirmName,
+            toLocation: toLocation
+        };
+        const transportsAsBytes = await ctx.stub.getState(batchesTransports);
+        const transports = JSON.parse(transportsAsBytes.toString());
+        transports[batchId] = newTransport;
+        await ctx.stub.putState(
+            batchesTransports,
+            Buffer.from(JSON.stringify(transports))
+        );
+        ctx.stub.setEvent(
+            "milkBatchTransported",
+            Buffer.from(
+                JSON.stringify({
+                    [batchId]: newTransport
+                })
+            )
+        );
 
-  /**
-   * Register that a new milk carton was transported
-   * @param {string} distributorName name of the distributor
-   * @param {string} toLocation place where the carton was transported to
-   * @param {string} cartonId identifier of the carton
-   */
-  async transportCarton(ctx, distributorName, toLocation, cartonId) {
-    console.info('============= START : transportCarton ===========');
+        console.info("============= END   : transportMilkBatch ===========");
+    }
 
-    const cartonsTransports = 'cartonsTransports';
-    const newTransport = {
-      distributorName: distributorName,
-      toLocation: toLocation,
-    };
-    const transportsAsBytes = await ctx.stub.getState(cartonsTransports);
-    const transports = JSON.parse(transportsAsBytes.toString());
-    transports[cartonId] = newTransport;
-    await ctx.stub.putState(cartonsTransports, Buffer.from(JSON.stringify(transports)));
-    ctx.stub.setEvent(
-      'cartonTransported',
-      Buffer.from(
-        JSON.stringify({
-          [cartonId]: newTransport,
-        }),
-      ),
-    );
+    /**
+     * Register a new milk carton, assumed that at the processing facility, milk from multiple batches is mixed, therefore, a single milk carton could contain milk of multiple batches
+     * @param {string} processorFirmName name of the firm that has processed the milk carton
+     * @param {string} location place where the processing has taken place
+     * @param {string} originBatchIds identifiers of the batches from which the milk was taken
+     * @param {string} cartonId identifier of the new milk carton
+     */
+    async registerMilkCarton(
+        ctx,
+        processorFirmName,
+        location,
+        originBatchIds,
+        cartonId
+    ) {
+        console.info("============= START : registerMilkCarton ===========");
 
-    console.info('============= END   : transportCarton ===========');
-  }
+        const cartonsRegister = "cartonsRegister";
+        const newCarton = {
+            processorFirmName: processorFirmName,
+            location: location,
+            originBatchIds: originBatchIds
+        };
+        const cartonsAsBytes = await ctx.stub.getState(cartonsRegister);
+        const cartons = JSON.parse(cartonsAsBytes.toString());
+        cartons[cartonId] = newCarton;
+        await ctx.stub.putState(
+            cartonsRegister,
+            Buffer.from(JSON.stringify(cartons))
+        );
+        ctx.stub.setEvent(
+            "cartonRegistered",
+            Buffer.from(
+                JSON.stringify({
+                    [cartonId]: newCarton
+                })
+            )
+        );
 
-  /**
-   * Register that a milk carton was stored in the inventory
-   * @param {string} retailerName name of the reatiler who will sell the carton
-   * @param {string} cartonId identifier of the carton
-   */
-  async registerCartonInInventory(ctx, retailerName, cartonId) {
-    console.info('============= START : registerCartonInInventory ===========');
+        console.info("============= END   : registerMilkCarton ===========");
+    }
 
-    const cartonsInventory = 'cartonsInventory';
-    const inventoryAsBytes = await ctx.stub.getState(cartonsInventory);
-    const inventory = JSON.parse(inventoryAsBytes.toString());
-    inventory[cartonId] = retailerName;
-    await ctx.stub.putState(cartonsInventory, Buffer.from(JSON.stringify(inventory)));
-    ctx.stub.setEvent(
-      'cartonRegisteredInInventory',
-      Buffer.from(
-        JSON.stringify({
-          [cartonId]: retailerName,
-        }),
-      ),
-    );
+    /**
+     * Register that a new milk carton was transported
+     * @param {string} distributorName name of the distributor
+     * @param {string} toLocation place where the carton was transported to
+     * @param {string} cartonId identifier of the carton
+     */
+    async transportCarton(ctx, distributorName, toLocation, cartonId) {
+        console.info("============= START : transportCarton ===========");
 
-    console.info('============= END   : registerCartonInInventory ===========');
-  }
+        const cartonsTransports = "cartonsTransports";
+        const newTransport = {
+            distributorName: distributorName,
+            toLocation: toLocation
+        };
+        const transportsAsBytes = await ctx.stub.getState(cartonsTransports);
+        const transports = JSON.parse(transportsAsBytes.toString());
+        transports[cartonId] = newTransport;
+        await ctx.stub.putState(
+            cartonsTransports,
+            Buffer.from(JSON.stringify(transports))
+        );
+        ctx.stub.setEvent(
+            "cartonTransported",
+            Buffer.from(
+                JSON.stringify({
+                    [cartonId]: newTransport
+                })
+            )
+        );
 
-  /**
-   * Register a new sale
-   * @param {string} cartonId identifier of the carton
-   */
-  async registerSelling(ctx, cartonId) {
-    console.info('============= START : registerSelling ===========');
+        console.info("============= END   : transportCarton ===========");
+    }
 
-    const cartonsSales = 'cartonsSales';
-    const salesAsBytes = await ctx.stub.getState(cartonsSales);
-    const sales = JSON.parse(salesAsBytes.toString());
-    sales.push(cartonId);
-    await ctx.stub.putState(cartonsSales, Buffer.from(JSON.stringify(sales)));
-    ctx.stub.setEvent('cartonSold', Buffer.from(cartonId));
+    /**
+     * Register that a milk carton was stored in the inventory
+     * @param {string} retailerName name of the reatiler who will sell the carton
+     * @param {string} cartonId identifier of the carton
+     */
+    async registerCartonInInventory(ctx, retailerName, cartonId) {
+        console.info(
+            "============= START : registerCartonInInventory ==========="
+        );
 
-    console.info('============= END   : registerSelling ===========');
-  }
+        const cartonsInventory = "cartonsInventory";
+        const inventoryAsBytes = await ctx.stub.getState(cartonsInventory);
+        const inventory = JSON.parse(inventoryAsBytes.toString());
+        inventory[cartonId] = retailerName;
+        await ctx.stub.putState(
+            cartonsInventory,
+            Buffer.from(JSON.stringify(inventory))
+        );
+        ctx.stub.setEvent(
+            "cartonRegisteredInInventory",
+            Buffer.from(
+                JSON.stringify({
+                    [cartonId]: retailerName
+                })
+            )
+        );
+
+        console.info(
+            "============= END   : registerCartonInInventory ==========="
+        );
+    }
+
+    /**
+     * Register a new sale
+     * @param {string} cartonId identifier of the carton
+     */
+    async registerSelling(ctx, cartonId) {
+        console.info("============= START : registerSelling ===========");
+
+        const cartonsSales = "cartonsSales";
+        const salesAsBytes = await ctx.stub.getState(cartonsSales);
+        console.log(salesAsBytes.toString());
+        const sales = JSON.parse(salesAsBytes.toString());
+        sales.push(cartonId);
+        await ctx.stub.putState(
+            cartonsSales,
+            Buffer.from(JSON.stringify(sales))
+        );
+        ctx.stub.setEvent("cartonSold", Buffer.from(cartonId));
+
+        console.info("============= END   : registerSelling ===========");
+    }
 }
 
 module.exports = diary;
