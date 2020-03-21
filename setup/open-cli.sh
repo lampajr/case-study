7b1873abc60f8644965a3e811ecdbaee33b0bd5f
lampajr <lampa9559@gmail.com>
2020-03-21 12:56:56 +0100

    clisc-based client application scripts implemented

---
+++ setup/open-cli.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+#
+# Copyright Andrea Lamparelli Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Open clisc-based client application,"
+echo
+
+sudo docker exec -it cli-app bash
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0
